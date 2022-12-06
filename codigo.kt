4a9b2a1142ed03f5c205fef74b9cfcf3630229f9
Fabian <36656881+FRNalexander@users.noreply.github.com>
2022-12-05 21:43:51 -0500

    Update codigo.kt

--- codigo.kt
+++ codigo.kt
@@ -1,3 +1,1 @@
-//Hilt
-    implementation "com.google.dagger:hilt-android:2.44"
-    kapt "com.google.dagger:hilt-compiler:2.44"
+<uses-sdk tools:overrideLibrary="com.google.zxing.client.android" />
