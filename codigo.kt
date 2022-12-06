be885ac64cd1deb65bdc8bce7e25af6dbebbcd78
Fabian <36656881+FRNalexander@users.noreply.github.com>
2022-12-05 21:26:00 -0500

    Update codigo.kt

--- codigo.kt
+++ codigo.kt
@@ -1,3 +1,2 @@
-//zxing
-implementation 'com.google.zxing:core:3.3.0'
-implementation('com.journeyapps:zxing-android-embedded:4.3.0') {transitive = false}
+//Logger
+implementation 'com.orhanobut:logger:2.2.0'
