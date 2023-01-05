0047149d73b3442c161fe99dc16cf3fe4101e95a
Fabian <36656881+FRNalexander@users.noreply.github.com>
2023-01-04 21:27:39 -0500

    Update codigo.kt

--- codigo.kt
+++ codigo.kt
@@ -1,3 +1,5 @@
-<activity android:name="com.journeyapps.barcodescanner.CaptureActivity"
-            android:screenOrientation="portrait"
-            tools:replace="screenOrientation"/>
+sealed class Resource<out T> {
+    class Loading<out T> : Resource<T>()
+    data class Success<out T>(val data: T) : Resource<T>()
+    data class Failure<out T>(val messaage: String, val exception: Exception) : Resource<T>()
+}
